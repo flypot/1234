24acb3be34927619b73c6b8192012f82eca529cf
flypot <54131750+flypot@users.noreply.github.com>
2022-12-09 01:36:13 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,13 @@
+src="//maps.googleapis.com/maps/api/js?key=AIzaSyDJW4jsPlNKgv6jFm3B5Edp5ywgdqLWdmc&sensor=false"
+
+function initialize()
+{
+var mapOpt = {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+var map=new google.maps.Map(document.getElementById("googleMap"),mapOpt);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
